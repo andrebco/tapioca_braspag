6da7f6450910eaa8871cdf4554141a23ceffe569
Rômulo Collopy <romulocollopy@gmail.com>
2015-09-16 01:11:02 -0300

    post to braspag and return 400

--- tapioca_braspag/tapioca_braspag.py
+++ tapioca_braspag/tapioca_braspag.py
@@ -3,19 +3,19 @@
 from tapioca import (
     TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
 
+from tapioca.tapioca import TapiocaClient, TapiocaClientExecutor
 
 from resource_mapping import RESOURCE_MAPPING
 
 
 class BraspagClientAdapter(JSONAdapterMixin, TapiocaAdapter):
-    api_root = 'https://github.com/parafernalia/tapioca_braspag'
+    api_root = 'https://apisandbox.braspag.com.br/'
+    api_get_root = 'https://apiquerysandbox.braspag.com.br/'
     resource_mapping = RESOURCE_MAPPING
 
     def get_request_kwargs(self, api_params, *args, **kwargs):
         params = super(BraspagClientAdapter, self).get_request_kwargs(
             api_params, *args, **kwargs)
-
-        
 
         return params
 
@@ -24,7 +24,39 @@ class BraspagClientAdapter(JSONAdapterMixin, TapiocaAdapter):
 
     def get_iterator_next_request_kwargs(self,
             iterator_request_kwargs, response_data, response):
-        pass
+        if not paging:
+            return
+        url = paging.get('next')
+
+        if url:
+            return {'url': url}
+
+    def process_response(self, response):
+        data = self.response_to_native(response)
+        return data
 
 
-Braspag = generate_wrapper_from_adapter(BraspagClientAdapter)
+class TapiocaBraspagInstantiator(object):
+
+    def __init__(self, adapter_class):
+        self.adapter_class = adapter_class
+
+    def __call__(self, *args, **kwargs):
+        return TapiocaBraspagClient(self.adapter_class(), api_params=kwargs)
+
+
+class TapiocaBraspagClient(TapiocaClient):
+
+    def _wrap_in_tapioca_executor(self, data, *args, **kwargs):
+        return TapiocaBraspagClientExecutor(self._api.__class__(),
+            data=data, api_params=self._api_params, *args, **kwargs)
+
+
+class TapiocaBraspagClientExecutor(TapiocaClientExecutor):
+
+    def get(self, *args, **kwargs):
+        kwargs.update({'url': self._api.api_get_root})
+        return self._make_request('GET', *args, **kwargs)
+
+
+Braspag = TapiocaBraspagInstantiator(BraspagClientAdapter)
--- tests/test_tapioca_braspag.py
+++ tests/test_tapioca_braspag.py
@@ -3,12 +3,47 @@
 import unittest
 
 from tapioca_braspag import Braspag
-
+import decouple
 
 class TestTapiocaBraspag(unittest.TestCase):
 
     def setUp(self):
-        self.wrapper = Braspag()
+        headers = {
+            "Content-Type": "application/json",
+            "MerchantId": ""
+        }
+        self.wrapper = Braspag(headers=headers)
+
+    def test_post_return_200(self):
+        post_data = {
+           "MerchantOrderId": "2014111703",
+           "Customer": {
+              "Name": "Comprador Teste"
+           },
+           "Payment":{
+             "Type": "CreditCard",
+             "Amount": 15700,
+             "Provider": "Simulado",
+             "Installments": 1,
+             "CreditCard": {
+                 "CardNumber": "0000.0000.0000.0001",
+                 "Holder": "Teste Holder",
+                 "ExpirationDate": "12/2021",
+                 "SecurityCode": "XXX",
+                 "Brand": "Visa"
+             }
+           }
+        }
+
+        exp_resp = [
+                {
+                    "Code": 101,
+                    "Message": "MerchantId is required"
+                }
+            ]
+
+        resp = self.wrapper.sales_create().post(data=post_data)
+        self.assertEqual(exp_resp, resp().data())
 
 
 if __name__ == '__main__':
